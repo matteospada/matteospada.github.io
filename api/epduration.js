e319daa5422c259f0a1b304d194778bc4398c134
Matteo Spada <matteo.spada@admiral-eu.com>
2019-12-15 17:18:03 +0100

    modificato il file epduration.js

--- api/epduration.js
+++ api/epduration.js
@@ -1,19 +1,20 @@
-function getURL() {
-    var url = window.location.href;
-    var mp3Path = getSecondPart(url)
-    var au = document.createElement('audio');
-    au.src = mp3Path
-    au.addEventListener('loadedmetadata', function () {
+document.addEventListener('DOMContentLoaded', () => {
+        var url = window.location.href;
+        var mp3Path = getSecondPart(url)
+        var au = document.createElement('audio');
+        au.src = mp3Path
+        au.addEventListener('loadedmetadata', function () {
 
-        var duration = au.duration;
-        var node = document.createElement("p");                
-        var textnode = document.createTextNode(duration);   
-        node.appendChild(textnode);                           
-        document.body.appendChild(node);
+            var duration = au.duration;
+            var node = document.createElement("p");
+            var textnode = document.createTextNode(duration);
+            node.appendChild(textnode);
+            document.body.appendChild(node);
 
-    }, false);
-    document.body.innerHTML
-}
+        }, false);
+        document.body.innerHTML
+
+});
 
 function getSecondPart(str) {
     return str.split('?')[1];
